306448a73efe06e621792fd50480b8f582b97161
oohlearn <learnooh@gmail.com>
2024-04-21 21:46:01 +0800

    sign up finished

--- trytrydjango/trytrydjango/urls.py
+++ trytrydjango/trytrydjango/urls.py
@@ -19,11 +19,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from core.views import index, contact
-
 urlpatterns = [
-    path("", index, name="index"),
+    path("", include("core.urls")),
     path("items/", include("item.urls")),  #引入items裡面的urls.py
     path('admin/', admin.site.urls),
-    path("contact/", contact, name="contact")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
