0df259bb3e9a660705e434d2250107648dc2d107
oohlearn <learnooh@gmail.com>
2024-04-25 18:03:38 +0800

    add dashboard and delete function

--- trytrydjango/trytrydjango/urls.py
+++ trytrydjango/trytrydjango/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("", include("core.urls")),
     path("items/", include("item.urls")),  #引入items裡面的urls.py
     path('admin/', admin.site.urls),
+    path("dashboard/", include("dashboard.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
